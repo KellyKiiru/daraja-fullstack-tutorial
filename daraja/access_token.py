6b9649f6957611b4bbf4fadb5ee29cb504b33be2
kellykiiru <infowithkiiru@gmail.com>
2023-10-02 11:13:13 +0300

    generate_access_token code

---
+++ daraja/access_token.py
@@ -0,0 +1,20 @@
+import requests
+from requests import auth
+from requests.auth import HTTPBasicAuth
+
+import keys
+
+
+def generate_access_token():
+    
+    res = requests.get(keys.ACCESS_TOKEN_URL, auth=HTTPBasicAuth(keys.CONSUMER_KEY, keys.CONSUMER_SECRET))
+    
+    json_response = res.json()
+    
+    access_token = json_response["access_token"]
+    
+    print(access_token)
+    
+    return access_token
+
+generate_access_token()
