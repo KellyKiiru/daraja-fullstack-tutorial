74a96dac77bdb81c939a741678a8bf2b27604ff5
kellykiiru <infowithkiiru@gmail.com>
2023-10-02 10:02:28 +0300

    format time

--- daraja/lipanampesa.py
+++ daraja/lipanampesa.py
@@ -1,24 +1,34 @@
 import requests
+import base64
+from datetime import datetime
 
 from . import keys
-access_token = "access-token"
-api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-headers = {"Authorization": "Bearer %s" % access_token}
 
-request = {
-    "BusinessShortCode": keys.BUSINESS_SHORT_CODE,
-    "Password": "<PASSWORD>",
-    "Timestamp": "1588835661",
-    "TransactionType": "CustomerPayBillOnline",
-    "Amount": "1",
-    "PartyA":keys.PARTY_A,
-    "PartyB":keys.BUSINESS_SHORT_CODE,
-    "PhoneNumber":"",
-    "CallBackUrl":"http://127.0.0.1:8000/",
-    "AccountReference":"ID-number",
-    "TransactionDesc":"",
-}
+unformatted_time = datetime.now()
+formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
 
-response = requests.post(api_url, headers=headers, json=request)
 
-print(response.json())
+
+
+def lipanampesa():
+    access_token = "access-token"
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+
+    request = {
+        "BusinessShortCode": keys.BUSINESS_SHORT_CODE,
+        "Password": "<PASSWORD>",
+        "Timestamp": "1588835661",
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": "1",
+        "PartyA":keys.PARTY_A,
+        "PartyB":keys.BUSINESS_SHORT_CODE,
+        "PhoneNumber":"",
+        "CallBackUrl":"http://127.0.0.1:8000/",
+        "AccountReference":"ID-number",
+        "TransactionDesc":"",
+    }
+
+    response = requests.post(api_url, headers=headers, json=request)
+
+    print(response.json())
